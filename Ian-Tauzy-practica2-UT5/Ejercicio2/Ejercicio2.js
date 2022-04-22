c44dffa894765446f0e0c6e929dd520879bd8e12
Naiian6 <94165922+Naiian6@users.noreply.github.com>
2022-04-22 17:24:29 +0200

    Ejercicio2-CasiTerminado

---
+++ Ian-Tauzy-practica2-UT5/Ejercicio2/Ejercicio2.js
@@ -0,0 +1,25 @@
+'use strict'
+
+function gestionarFicheroTXT(txtDoc) {
+    //alert(txtDoc)
+    //console.log(txtDoc.split("\n"))
+    let mainHtml = document.querySelector("main")
+    let cadena = ""
+    let mensajes = txtDoc.split("\n")
+    cadena += "<ul>"
+        for(let i = 0; i < mensajes.length; i++)
+        {
+            if (i%2 == 0) {
+                cadena += "<li class='emisor'>" + mensajes[i] + "</li>"
+            }
+            else cadena += "<li class='receptor'>" + mensajes[i] + "</li>"
+        }
+    cadena += "</ul>"
+    
+    mainHtml.innerHTML = cadena
+}
+
+
+loadLDocA("Mensajes.txt")
+//setTimeout(()=>loadLDocA("Mensajes.txt"),3000) 
+//setInterval()
