9726ca16d2f60130c933ac7b32919ecac1a3a653
Naiian6 <94165922+Naiian6@users.noreply.github.com>
2022-05-18 00:42:18 -0800

    Clase Pokemon Terminada

--- Ian-Tauzy-Actividad3-UT5/JS/Pokemon.class.js
+++ Ian-Tauzy-Actividad3-UT5/JS/Pokemon.class.js
@@ -1,27 +1,28 @@
 "use strict";
-
-//let miPokemon = new Object();
-
 class Pokemon
 {
-    constructor(imagen, nombre, experiencia, ataque)
+    constructor(imagen, nombre, experiencia, ataque, defensa, vida)
     {
         this.imagen = imagen;
         this.nombre = nombre;
         this.experiencia = experiencia;
         this.ataque = ataque;
+        this.defensa = defensa;
+        this.hp = vida;
     }
-    static getPokemon()
+    static getPokemon(numRandom)
     {
         return HTTP.ajax('GET',
-        `https://pokeapi.co/api/v2/pokemon/` + getRandomInt(0,898)).then(
+        `https://pokeapi.co/api/v2/pokemon/` + numRandom).then(
         data => {
             console.log(data)
             let name = data.name
             let Pkimg = data.sprites.front_default
             let Pkexperiencia = data.base_experience
             let Pkataque = data.stats[1].base_stat
-            let pk = new Pokemon(Pkimg,name,Pkexperiencia,Pkataque);
+            let Pkdefensa = data.stats[2].base_stat
+            let Pkhp = data.stats[0].base_stat
+            let pk = new Pokemon(Pkimg,name,Pkexperiencia,Pkataque,Pkdefensa,Pkhp);
             return pk;
         })
     }
@@ -30,7 +31,8 @@ class Pokemon
         document.querySelector("img").src = this.imagen;
         document.querySelector("h4").textContent = this.nombre;
         document.querySelectorAll("p")[0].textContent = this.ataque;
-        document.querySelectorAll("p")[1].textContent = this.experiencia;
+        document.querySelectorAll("p")[1].textContent = this.defensa;
+        document.querySelectorAll("p")[2].textContent = this.experiencia;
+        document.querySelector("h5").textContent = this.hp + " HP";
     }
-
 }
